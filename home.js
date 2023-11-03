3fd59a5e6fa00a86df2f987e3527c56dd71c3161
Abishek P <139522751+shek123abi@users.noreply.github.com>
2023-11-03 09:42:00 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export function Home(){
+    return(
+        <div className="container my-5">
+            <h2 className="text-center mb-3">Home page</h2>
+        </div>
+    )
+}
---
+++ index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Navbar,Footer } from './pages/layout';
+import { Home } from './pages/home';
+import { Products } from './pages/products';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+function App(){
+  return(
+    <>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/products" element={<Products/>}/>
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
+    
+    </>
+    
+  )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
